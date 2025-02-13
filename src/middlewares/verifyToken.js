3040bd54b0eb781dfeba8c357dd5689bb12eae77
Pedro Fernandez <pefernandez1408@gmail.com>
2025-02-13 00:10:07 -0400

    middleware de verificacion de token

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+export function verifyToken(req, res, next) {
+    const token = req.header('acces_token');
+
+    if (!token) {
+        return res.status(401).json({ mensaje: 'No se ha proporcionado un token' });
+    }
+    try {
+        const decoded = jwt.verify(token, 'my-secret')
+
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(401).json({ mensaje: 'Token inválido' });
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { userControllers } from "../controllers/user.controllers.js";
+import { verifyToken } from "../middlewares/verifyToken.js"
 
 const route = Router()
 
 route.post('/register', userControllers.createUser)
-route.post('/login', userControllers.login)
+route.post('/login', verifyToken, userControllers.login)
 
 export default route
